0a4a1a0364db84223c5f3a461c408bebdb1ae671
Julian Miller <73328279+docjulz@users.noreply.github.com>
2022-04-18 15:05:52 -0700

    changed button functionality

--- setup/app.js
+++ setup/app.js
@@ -1,11 +1,31 @@
-//using selectors inside the element
-// traversing the dom
+//functionality for buttons. Open or close one or all
 
-const buttons = document.querySelectorAll('.question-btn');
+// const buttons = document.querySelectorAll('.question-btn');
 
-buttons.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle('show-text');
-    })
-})
+// buttons.forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
+
+
+// Opens only one question at a time.
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+
+    const btn = question.querySelector('.question-btn');
+
+    btn.addEventListener('click', (item) => {
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove('show-text')
+            }
+        });
+
+        question.classList.toggle ('show-text')
+
+    });    
+    
+});
